ac442bf943f1ae7068520c065c363f9cd3810f5d
Neelesh Gamage <neeleshgamage2004@gmail.com>
2026-07-31 18:39:00 +0530

    added count vowels function

---
+++ src/main.py
@@ -0,0 +1,8 @@
+def count_vowels(input_string):
+    vowels = 'aeiouAEIOU'
+    return sum(1 for char in input_string if char in vowels)
+
+input_string = "Hello, World!"
+vowel_count = count_vowels(input_string)
+print(f"The number of vowels in '{input_string}' is {vowel_count}.")
+
