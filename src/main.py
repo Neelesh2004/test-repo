b6b214ba89805800313b2bc40f9408a7878fefd2
Neelesh Gamage <neeleshgamage2004@gmail.com>
2026-07-31 18:42:02 +0530

    changed input string from main branch

--- src/main.py
+++ src/main.py
@@ -2,7 +2,7 @@ def count_vowels(input_string):
     vowels = 'aeiouAEIOU'
     return sum(1 for char in input_string if char in vowels)
 
-input_string = "Hello, World!"
+input_string = "Neelesh"
 vowel_count = count_vowels(input_string)
 print(f"The number of vowels in '{input_string}' is {vowel_count}.")
 
